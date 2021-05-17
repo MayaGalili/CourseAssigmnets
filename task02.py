50d15b7b1e5cd0040c9bbcdb040c54448b8e6484
MayaGalili <mayavav@gmail.com>
2021-05-17 17:29:59 +0300

    Task 2 - final solution

--- task02.py
+++ task02.py
@@ -1,19 +1,58 @@
 import sys
-import numpy as np
 
-a, b = map(int, sys.stdin.readline().split())
+"""
+TASK 02 - 
+Merge two sorted arrays into one sorted array.
 
-assert 0 <= a <= 10000000
-assert 0 <= b <= 10000000
-
-ta = [int(sys.stdin.readline()) for _ in range(a)]
-tb = [int(sys.stdin.readline()) for _ in range(b)]
-
-pivot_num = 0
+"""
 
 
-to_sort = np.unique(ta + tb)
-sorted_lst = sorted(to_sort)
+def run():
+    a, b = map(int, sys.stdin.readline().split())
+    assert 0 <= a <= 10000000
+    assert 0 <= b <= 10000000
 
-for nm in sorted_lst:
-  print(nm)
+    ta = [int(sys.stdin.readline()) for _ in range(a)]
+    tb = [int(sys.stdin.readline()) for _ in range(b)]
+
+    ai = 0
+    bi = 0
+
+    assert 0 <= ta[0] and ta[-1] <= 1000000000
+    assert 0 <= tb[0] and tb[-1] <= 1000000000
+
+    while ai < a and bi < b:
+        if ta[ai] > tb[bi]:
+            print(tb[bi])
+            bi += 1
+
+        elif tb[bi] > ta[ai]:
+            print(ta[ai])
+            ai += 1
+
+        elif tb[bi] == ta[ai]:
+            print(ta[ai])
+            ai += 1
+            bi += 1
+
+    while (ai >= a and bi < b) or (ai < a and bi >= b):
+
+        if ai < a and bi >= b:
+            print(ta[ai])
+            ai += 1
+
+        if bi < b and ai >= a:
+            print(tb[bi])
+            bi += 1
+
+
+if __name__ == "__main__":
+    """
+    2 3
+    1
+    3
+    2
+    4
+    5
+    """
+    run()
